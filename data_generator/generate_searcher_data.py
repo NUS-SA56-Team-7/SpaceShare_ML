b6e68537f58bf79d596dbfc789d82c380ad5eddb
leroytantingen@gmail.com <leroytantingen@gmail.com>
2023-08-17 14:39:25 +0800

    v0.5 change the output format

--- data_generator/generate_searcher_data.py
+++ data_generator/generate_searcher_data.py
@@ -2,11 +2,19 @@ import types
 import numpy as np
 import pandas as pd
 import random as rd
+import uuid
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import pickle
 import json
 import os
+ROOM_TYPE = ["SINGLE","MASTER","COMMON","WHOLE_UNIT"]
+PROPERTY_TYPE = ["HDB","CONDOMINIUM","LANDED"]
 def seacher_data(n,id):
-    data = [[id,rd.randint(0,2),rd.randint(0,3)] for i in range(n)]
-    return pd.DataFrame(data=data,columns=["id","property_type","room_type"])
+    data = [[id,uuid.uuid1(),PROPERTY_TYPE[rd.randint(0,2)],ROOM_TYPE[rd.randint(0,3)]] for i in range(n)]
+    return pd.DataFrame(data=data,columns=["tenant_id","property_id","property_type","room_type"])
 def generate_data(n=None,id=None,df=None):
     if isinstance(df, types.NoneType) == True:
         df= seacher_data(n,id)
@@ -17,5 +25,83 @@ def generate_data(n=None,id=None,df=None):
     for x in np.unique(df["room_type"].values):
         room_type[x] = df["room_type"].value_counts()[x]
     return property_type+room_type
-test = generate_data(1,1)
-print(test)
+def df_to_freq(df,id,property_unique=[0,1,2],room_unique=[0,1,2,3]):
+    freq_room = [0]*4
+    freq_property =[0]*3
+    count_property = df["property_type"].value_counts()
+    for unique in property_unique:
+        try:
+            freq_property[int(unique)]=count_property[int(unique)]
+        except:
+            pass
+    freq_property = [x/len(df) for x in freq_property]
+    count_room = df["room_type"].value_counts()
+    for unique in room_unique:
+        try:
+            freq_room[int(unique)]=count_room[int(unique)]
+        except:
+            pass
+    freq_room = [x/len(df) for x in freq_room]
+    return [id]+freq_property+freq_room
+def data_to_cluster(df,label):
+    freqs = []
+    for i in np.unique(df['tenant_id']):
+        freqs.append(df_to_freq(df=df[df["tenant_id"]==i],id=i))
+    df_freq=pd.DataFrame(data=freqs,
+                        columns=["tenant_id","CONDOMINIUM","HDB","LANDED","COMMON","MASTER","SINGLE","WHOLE_UNIT"]
+                        ).set_index("tenant_id")
+    datas = [list(x) for x in zip(df_freq.index.tolist(),label)]
+    return pd.DataFrame(data=datas,columns=["id","cluster"])
+
+def main():
+    dfs = []
+    for i in range(1000):
+        df = seacher_data(rd.randint(1,20),i)
+        dfs.append(df)
+    df_final = pd.concat(dfs,ignore_index=True)
+    df_final.to_csv("./dataset/mock_data.csv")
+    df_property_list = df_final[["property_id","property_type","room_type"]]
+    df_property_list.to_csv("./dataset/mock_property.csv")
+    encoder = OrdinalEncoder()
+    df_property_list[["property_type","room_type"]] = encoder.fit_transform(df_property_list[["property_type","room_type"]])
+    df_property_list=df_property_list.set_index("property_id")
+    print(df_property_list)
+    df_final[["property_type","room_type"]]=encoder.transform(df_final[["property_type","room_type"]])
+    print(df_final)
+    property_unique = np.unique(df_final["property_type"])
+    room_unique= np.unique(df_final["room_type"])
+    freqs = []
+    for i in np.unique(df_final["tenant_id"]):   
+        freqs.append(df_to_freq(df=df_final[df_final["tenant_id"]==i],id=i))
+    df_freq=pd.DataFrame(data=freqs,
+                        columns=["tenant_id","CONDOMINIUM","HDB","LANDED","COMMON","MASTER","SINGLE","WHOLE_UNIT"]
+                        ).set_index("tenant_id")
+    print(df_freq)
+    pc = PCA(2)
+    cluster = KMeans(n_clusters=12)
+    df_fit = pc.fit_transform(df_freq)
+    print(df_fit)
+    label = cluster.fit_predict(df_fit)
+    test = cluster.predict(pc.transform([[0.5,0.25,0.25,0.5,0.1,0.2,0.3]]))
+    u_labels = np.unique(label)  
+    for i in u_labels:
+        plt.scatter(df_fit[label == i , 0] , df_fit[label == i , 1] , label = i)
+    plt.legend()
+    plt.show()
+    datas = [list(x) for x in zip(df_freq.index.tolist(),label)]
+    print(label)
+    print(df_freq.index.to_list())
+    print(datas)
+    df_cluster = pd.DataFrame(data=datas,columns=["id","cluster"])
+    print(df_cluster[df_cluster.iloc[:,1]==1].iloc[:,0])
+    print(df_final[df_final["tenant_id"].isin( df_cluster[df_cluster.iloc[:,1]==1].iloc[:,0].values)]["property_id"].sample(5))
+    with open("./models/cluster_model.pkl","wb") as f:
+        pickle.dump(cluster,f)
+    with open("./models/cluster_encoder.pkl","wb") as f:
+        pickle.dump(encoder,f)
+    with open("./models/pca.pkl", "wb") as f:
+        pickle.dump(pc,f)
+    print(data_to_cluster(df_final,label=label))
+if __name__ == "__main__":
+    main()
+    
--- deployment_model/main.py
+++ deployment_model/main.py
@@ -1,5 +1,6 @@
 import pickle as Pickle
 import sys
+import requests
 import numpy as np
 sys.path.append("./")
 import data_generator.generate_searcher_data as gd
@@ -7,39 +8,59 @@ import pandas as pd
 from flask import Flask, jsonify, request
 from waitress import serve
 import json
-with open("./models/model.pkl",mode="rb") as f:
+with open("./models/cluster_model.pkl",mode="rb") as f:
     model = Pickle.load(f)
-with open("./models/scaler.pkl",mode="rb") as f:
-    scaler = Pickle.load(f)
+with open("./models/cluster_encoder.pkl",mode="rb") as f:
+    encoder = Pickle.load(f)
+with open("./models/pca.pkl",mode= "rb") as f:
+    scalar = Pickle.load(f)
+df_history = pd.read_csv("./dataset/mock_data.csv",index_col=0)
+df_property = pd.read_csv("./dataset/mock_property.csv",index_col=0)
+#for loading of df from backend
+#response = requests.post()
+#property_json = json.loads(response)
+#property_df = pd.json_normalize(property_json)
+df_cluster = pd.read_csv("./dataset/cluster_data.csv",index_col=0)
 app = Flask("__name__")
 
 @app.route("/api/analytics/classify",methods=['Post'])
 def clustering():
-    try:
-        user_data = request.form.get("userData") or request.args.get("userData")
-        user_data = json.loads(user_data)
-        
-
-    except:
-        return "Bad Request",400
-    df = pd.json_normalize(user_data)
-    if(len(df)!=20):
-        return "Bad Request",400
-    feature = gd.generate_data(df=df)
-    feature = scaler.transform([feature])
-    return jsonify(int(model.predict(feature)))
-@app.route("/api/analytics/classify/multiple",methods=['Post'])
-def multipleclustering():
-    try:
-        user_data = request.form.get("userData") or request.args.get("userData")
-        user_data = json.loads(user_data)
-    except:
-        return "Bad Request",400
-    df = pd.json_normalize(user_data)
-    features = [gd.generate_data(df=df[df["id"]==id])for id in np.unique(df["id"].values)]
-    features = scaler.transform(features)
-    return '{}'.format(','.join(str(x) for x in model.predict(features)))
-
-        
-
+   refresh()
+   id = request.form.get("id") or request.args.get("id")
+   df = df_history[df_history["tenant_id"]==int(id)]
+   df_list = gd.df_to_freq(df,id)
+   df_list.pop(0)
+   data = scalar.transform([df_list])
+   cluster = model.predict(data)
+   sim_cluster = df_cluster[df_cluster["cluster"]==cluster[0]]['id'].to_list()
+   property_reco = df_history[df_history["tenant_id"].isin(sim_cluster)]["property_id"]
+   return property_reco.sample(5).to_json()
+@app.route("/api/analytics/load_today_cluster",methods =['Get'])
+def cache():
+    refresh()
+    df_history[["property_type","room_type"]] = encoder.transform(df_history[["property_type","room_type"]])
+    data = []
+    for i in np.unique(df_history['tenant_id']):
+        data.append(gd.df_to_freq(df_history[df_history["tenant_id"]==i],i))
+    df = pd.DataFrame(data=data,columns=["tenant_id","CONDOMINIUM","HDB","LANDED","COMMON","MASTER","SINGLE","WHOLE_UNIT"]
+)
+    df = df.set_index("tenant_id")
+    features = scalar.transform(df.values)
+    label = model.predict(features)
+    df = gd.data_to_cluster(df_history,label)
+    df.to_csv("./dataset/cluster_data.csv")
+    return "Success", 200
+@app.route("/api/analytics/refresh")
+def refresh():
+    #response = requests.post()
+    #property_json = json.loads(response)
+    #property_df = pd.json_normalize(property_json)
+    #to reload the memory of new df
+    pass
+@app.route("/api/analytics/retrain")
+def retrain():
+    refresh()
+    #model.fit(new data)
+    #save model in pickle
+    pass
 serve(app)
